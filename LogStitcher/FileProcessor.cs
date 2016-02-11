5d4d0a4fd92b2747bb65558e4aabeb5af1811a8a
Josh Quintus <josh@dotalign.com>
2016-02-11 15:47:55 -0500

    Initial commit

---
+++ LogStitcher/FileProcessor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LogStitcher
+{
+    public class FileProcessor
+    {
+        public FileProcessor(string file)
+        {
+            File = new FileInfo(file);
+        }
+
+        public FileInfo File { get; }
+
+        public string GetErrors()
+        {
+            if (!File.Exists)
+            {
+                return $"File does not exist {File}";
+            }
+            return null;
+        }
+
+        public IEnumerable<Line> ReadLines()
+        {
+            using (var stream = File.OpenRead())
+            using (var sr = new StreamReader(stream))
+            {
+                string text = null;
+                var lastLine = new Line(string.Empty, File, null);
+                while ((text = sr.ReadLine()) != null)
+                {
+                    var curLine = new Line(text, File, lastLine);
+                    lastLine = curLine;
+                    yield return curLine;
+                }
+            }
+        }
+    }
+
+    public class Line
+    {
+        public Line(string text, FileInfo file, Line lastLine)
+        {
+            // sample line:  02/10/2016 11:42:44.511 | 91418 | INFO  | Outlook sync run completed
+
+            Text = text;
+            SourceName = file.Name;
+            var parts = text.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Date = parts.Select(p => TryGetDate(p)).FirstOrDefault() ?? lastLine?.Date ?? DateTime.MinValue;
+
+        }
+
+        public DateTime Date { get; }
+
+        public string SourceName { get; }
+
+        public string Text { get; }
+
+        private DateTime? TryGetDate(string text)
+        {
+            DateTime date;
+            if (DateTime.TryParse(text, out date))
+            {
+                return date;
+            }
+            return null;
+        }
+    }
+}
---
+++ LogStitcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace LogStitcher
+{
+    public class Program
+    {
+        public static IEnumerable<string> ExpandFilePaths(string arg)
+        {
+            var substitutedArg = System.Environment.ExpandEnvironmentVariables(arg);
+
+            var dirPart = Path.GetDirectoryName(substitutedArg);
+            if (dirPart.Length == 0)
+                dirPart = ".";
+
+            var filePart = Path.GetFileName(substitutedArg);
+
+            foreach (var filepath in Directory.GetFiles(dirPart, filePart))
+            {
+                yield return filepath;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine($"Usage:  {Process.GetCurrentProcess().ProcessName} [File1] [File2] ...");
+                return;
+            }
+
+            var processors = args.SelectMany(a => ExpandFilePaths(a)).Select(f => new FileProcessor(f)).ToList();
+
+            var allLines = processors.SelectMany(p => p.ReadLines()).OrderBy(l => l.Date);
+
+            foreach (var line in allLines)
+            {
+                Console.WriteLine($"{line.SourceName} | {line.Text}");
+            }
+        }
+    }
+
+
+}
